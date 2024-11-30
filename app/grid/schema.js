f27ba7a1679b18969e2016a28196c152660f85fb
majordomo-admin <admin@majordomo.systems>
2024-11-30 10:02:04 -0500

    Add comments to schema;

--- app/grid/schema.js
+++ app/grid/schema.js
@@ -1,12 +1,21 @@
 const schema = {
-  showToolbar: true, // Toggle toolbar
-  showFooter: true, // Toggle footer
-  selectedRowCount: false, // Toggle selected row count
+  // Toggle toolbar (true or false)
+  showToolbar: true,
+  // Toggle footer (true or false)
+  showFooter: true,
+  // Toggle selected row count (true or false)
+  selectedRowCount: false,
+  // Specify columns (keys) to display and their order
   // order: ['size', 'color', 'price', 'rating', 'stock'],
+  // Specify which column (key) to sort by default
   // defaultSorting: [{ key: 'category', desc: false }],
+  // Specify which columns (keys) are sortable
   sortableColumns: ['title', 'category', 'price'],
+  // Specify which columns (keys) can be filtered in the toolbar
   filters: ['category', 'size'],
+  // Specify which columns (keys) will display values in a badge component
   badgeColumns: ['price', 'shipping_cost'],
+  // Specify which columns (keys) should be center aligned
   centerAlignedColumns: [
     'category',
     'size',
