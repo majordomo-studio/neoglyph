ccaa64900e3e4f689b550e86e75f3c2adf44380b
majordomo-admin <admin@majordomo.systems>
2024-11-29 19:23:45 -0500

    Add view options;

--- components/DataGrid.js
+++ components/DataGrid.js
@@ -37,6 +37,8 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuCheckboxItem,
+  DropdownMenuLabel,
 } from '@/components/ui/dropdown-menu';
 import { DataTableFacetedFilter } from './DataTableFacetedFilter';
 
@@ -51,6 +53,7 @@ import {
   ChevronsUpDown,
   ArrowUp,
   ArrowDown,
+  Settings2,
 } from 'lucide-react'; // Import icons
 
 // Helper function to format column headers for display purposes
@@ -261,13 +264,46 @@ export default function DataGrid({ data = [], schema = null }) {
             );
           })}
         </div>
-        <Button
-          variant="ghost"
-          onClick={() => table.resetColumnFilters()}
-          className="h-8 px-2 lg:px-3"
-        >
-          Reset Filters
-        </Button>
+        <div className="flex items-center gap-2">
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset Filters
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm" className="flex h-8">
+                <Settings2 />
+                View
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[150px]">
+              <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {table
+                .getAllColumns()
+                .filter(
+                  (column) =>
+                    typeof column.accessorFn !== 'undefined' &&
+                    column.getCanHide()
+                )
+                .map((column) => (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     );
   };
