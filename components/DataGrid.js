1346aef4f3e18c419c1c167c9f26d2bc730a6fb4
majordomo-admin <admin@majordomo.systems>
2024-11-29 21:29:28 -0500

    Comment out selected row count;

--- components/DataGrid.js
+++ components/DataGrid.js
@@ -358,8 +358,8 @@ export default function DataGrid({ data = [], schema = null }) {
       </div>
       <div className="flex items-center justify-between px-2">
         <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{' '}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+          {/* {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected. */}
         </div>
         <div className="flex items-center space-x-6 lg:space-x-8">
           <div className="flex items-center space-x-2">
