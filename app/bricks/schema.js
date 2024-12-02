38c76cabab214efa8e0be7cab7c2f57a92163496
majordomo-admin <admin@majordomo.systems>
2024-12-02 01:31:10 -0500

    Update comments;

--- app/bricks/schema.js
+++ app/bricks/schema.js
@@ -1,13 +1,13 @@
 const schema = {
   // Specify keys to display in default view and their order
   order: ['size', 'color', 'price', 'rating', 'stock'],
-  // Specify which column (key) to sort by default
+  // Specify which key to sort by default
   // defaultSorting: [{ key: 'category', desc: false }],
   // Specify which keys are sortable
   sortable: ['title', 'category', 'price'],
   // Specify which keys can be filtered in the toolbar
   filters: ['category', 'size'],
-  // Specify which columns (keys) will display values in a badge component
+  // Specify which keys will display values in a badge component
   badges: ['price', 'shipping_cost'],
 };
 
